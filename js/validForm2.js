5f37ce9c947b94545944b1b218a3853f3b947e21
Winciwizard <vincent.verges@gmail.com>
2018-01-04 16:53:22 +0100

    version avec CCS 1ere étape

--- js/validForm2.js
+++ js/validForm2.js
@@ -11,15 +11,27 @@ $(document).ready(function(){
 
     var regexEmail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
 
-    $pass.keyup(function(){       
+    $pass.keyup(function(){
         if(($(this).val().length < 8) || ($(this).val().length > 16)){ // si la chaîne de caractères n'est pas un email.
-            $(this).removeClass("valide").addClass("erreur");
+            $(this).css({ // on rend le champ rouge
+                borderColor : '#d31d4e',
+                color : '#d31d4e',
+                boxShadow : '1px 1px 15px #d31d4e '
+            });
             $messagePass.text(' Mot de passe invalide');
-            $messagePass.addClass("erreurMessage");
+            $messagePass.css({
+                color : '#d31d4e',
+                fontWeight : 'bold',
+                fontSize : '0.9em'
+            });
          }
          else{
-            $(this).removeClass("erreur").addClass("valide");
-            $messagePass.text('');
+             $(this).css({ // si tout est bon, on le rend vert
+             borderColor : '#8cd98c',
+             color : '#8cd98c',
+             boxShadow : '1px 1px 15px #8cd98c '
+         });
+         $messagePass.text('');
          }
     });
 
